6f621029ba52046fec6e7134d796f877d93c7dc1
Azizallah Farhadi <115372006+AZFARHAD24511@users.noreply.github.com>
2025-07-03 19:05:16 +0200

    Update SHAP_bank_marketing_app.py

--- SHAP_bank_marketing_app.py
+++ SHAP_bank_marketing_app.py
@@ -33,8 +33,6 @@ if st.button("Run SHAP Analysis"):
 
         X = df.drop(columns=['y'])
         y = df['y'].map({'no': 0, 'yes': 1})
-        X = X.drop(columns=['contact', 'poutcome'])
-
         X_enc = X.copy()
         for col in X_enc.select_dtypes(include=['object']).columns:
             X_enc[col] = LabelEncoder().fit_transform(X_enc[col].astype(str))
